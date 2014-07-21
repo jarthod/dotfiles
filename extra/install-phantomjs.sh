d32a83fadbdd3ac80aa43ffdbbfd4a0c59b5ac8c
Adrien Jarthon <jobs@adrienjarthon.com>
2014-07-21 09:20:17 +0200

    update phantomjs setup

--- extra/install-phantomjs.sh
+++ extra/install-phantomjs.sh
@@ -1,4 +1,4 @@
-wget https://phantomjs.googlecode.com/files/phantomjs-1.9.0-linux-x86_64.tar.bz2
-tar -xjf phantomjs-1.9.0-linux-x86_64.tar.bz2
-sudo cp phantomjs-1.9.0-linux-x86_64/bin/phantomjs /usr/local/bin
-rm -R phantomjs-1.9.0-linux-x86_64*
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
+tar -xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
+sudo cp phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin
+rm -R phantomjs-1.9.7-linux-x86_64*
