56a08807e41cee018467de0633e1fdd8203887ab
Adrien Jarthon <jobs@adrienjarthon.com>
2014-07-21 09:15:55 +0200

    sublime text 3

--- extra/install-sublime-text.sh
+++ extra/install-sublime-text.sh
@@ -1,3 +1,8 @@
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
+# sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+
 sudo apt-get update
-sudo apt-get install sublime-text
+
+#sudo apt-get install sublime-text
+sudo apt-get install sublime-text-installer
+
