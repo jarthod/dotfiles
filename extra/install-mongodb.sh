ecfd500866958dd76c091c74c67fb1a881d4de07
Adrien Jarthon <jobs@adrienjarthon.com>
2017-03-12 17:34:26 +0100

    update mongo install script

--- extra/install-mongodb.sh
+++ extra/install-mongodb.sh
@@ -1,7 +1,5 @@
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-# Ubuntu 14.04
-# echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
 # Ubuntu 16.04 / debian
-echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
