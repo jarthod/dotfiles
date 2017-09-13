3f87eefb3663cd555f6b86b52d6292027ba8036c
Adrien Jarthon <jobs@adrienjarthon.com>
2017-09-13 23:33:20 +0200

    update sublime3 install script (official APT) + ssh config for faster SSH

--- extra/install-sublime-text.sh
+++ extra/install-sublime-text.sh
@@ -1,6 +1,4 @@
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-
-sudo apt-get update
-
-sudo apt-get install -y sublime-text-installer
-
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install sublime-text
