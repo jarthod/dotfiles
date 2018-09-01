743c2929fa73a214f7e2a0be48a5df714e8292e2
Adrien Jarthon <jobs@adrienjarthon.com>
2018-09-01 20:48:10 +0200

    remove extra hangout + add ncdu

--- extra/install-hangout.sh
+++
@@ -1,7 +0,0 @@
-sudo add-apt-repository ppa:nilarimogard/webupd8
-sudo apt update
-sudo apt install purple-hangouts pidgin-hangouts pidgin
-wget https://github.com/stv0g/unicode-emoji/archive/master.zip
-mkdir -p ~/.purple/smileys/
-unzip -q master.zip -d ~/.purple/smileys/
-rm master.zip
