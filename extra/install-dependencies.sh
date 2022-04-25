92ec5bb7532dd1118fbfadab2cdd67cb8929c944
Adrien Rey-Jarthon <jobs@adrienjarthon.com>
2022-04-25 10:24:44 +0200

    update install scripts for 22.04

--- extra/install-dependencies.sh
+++ extra/install-dependencies.sh
@@ -1,5 +1,1 @@
-sudo apt install -y ntp git htop iotop iftop atop build-essential screen curl autoconf zlib1g-dev nodejs libssl-dev libxml2-dev libxslt-dev libsqlite3-dev libreadline-dev python3.3 xvfb firefox redis-server imagemagick vnstat memcached libffi-dev nmap iperf libjemalloc-dev ncdu
-
-wget https://github.com/sharkdp/bat/releases/download/v0.6.1/bat_0.6.1_amd64.deb
-sudo dpkg -i bat_0.6.1_amd64.deb
-rm bat_0.6.1_amd64.deb
+sudo apt install -y ntp git htop iotop iftop atop build-essential screen curl autoconf zlib1g-dev nodejs libssl-dev libxml2-dev libxslt-dev libsqlite3-dev libreadline-dev python3 xvfb firefox redis-server imagemagick vnstat memcached libffi-dev nmap iperf libjemalloc-dev ncdu cmake gnome-tweaks
--- extra/install-sublime-text.sh
+++ extra/install-sublime-text.sh
@@ -1,4 +1,4 @@
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/trusted.gpg.d/sublime.asc
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
 sudo apt install sublime-text
